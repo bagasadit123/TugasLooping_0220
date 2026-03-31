8c76ba2a759f3a5c5541ecfd540eaaae512d25ff
bagasadit123 <bagasadit430@gmail.com>
2026-03-31 17:23:19 +0700

    prosedur untuk  mengecek bilangan prima

--- tugasLooping.cpp
+++ tugasLooping.cpp
@@ -8,3 +8,17 @@ void inputAngka() {
     cout << "Masukkan sebuah angka: ";
     cin >> n;
 }
+bool cekPrima(int x) {
+    if (x <= 1) {
+        return false;
+    }
+
+    int i = 2;
+    while (i < x) {
+        if (x % i == 0) {
+            return false;
+        }
+        i++;
+    }
+    return true;
+}
